cdbc647320494075dc20f1ac39aa577775204988
abyankamal <kamalmuhammad414@gmail.com>
2024-01-06 05:17:04 +0700

    Commit Ketiga

--- annotation/variable.ts
+++ annotation/variable.ts
@@ -8,7 +8,7 @@ const nothing : undefined = undefined;
 const date : Date = new Date();
 
 // array
-const numbers : number[] = [1,2,3,4,5,6,7,8,9,10];
+const numbersArray : number[] = [1,2,3,4,5,6,7,8,9,10];
 const names : string[] = ["amad", "jamal", "husein"];
 const booleans : boolean[] = [true, false];
 
@@ -35,12 +35,21 @@ const json = '{"x" = 10, "y" = 20}';
 const coordinates : {x: number, y: number} = JSON.parse(json);
 console.log(coordinates);
 
-// 2) lWhen we declare a variable on one line and iniziate it later
+// 2) When we declare a variable on one line and iniziate it later
 let words = ['red', 'green', 'blue'];
 let foundWords : boolean = false;
 
 for(let i = 0; i < words.length; i++){
     if(words[i] === 'green'){
         foundWords = true
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numbersAboveZero : boolean | number = false;
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > 0){
+        numbersAboveZero = numbers[i]
     }
 }
