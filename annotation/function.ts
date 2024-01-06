4e5e57b94cd7f494869952e6c11cb81694711096
abyankamal <kamalmuhammad414@gmail.com>
2024-01-07 05:26:40 +0700

    Commit Keempat

--- annotation/function.ts
+++ annotation/function.ts
@@ -1,3 +1,34 @@
-const add = (a: number, b: number) : number => {
+const add = (a: number, b: number) => {
     return a + b;
+}
+
+const subtract = (a: number, b: number) => {
+    return a - b;
+}
+
+function devider(a: number,b: number) {
+    return a / b;
+}
+
+const multiply = (a: number, b: number) => {
+    return a * b;
+}
+
+// void cannot return the value
+const logger = (messages: string) : void => {
+    return console.log(messages);
+}
+
+// never indicates the values that will never occur.
+const throwError = (messages: string) : never => {
+    throw new Error(messages);
+}
+
+const todaysWeather = {
+    date : new Date(),
+    weather : 'sunny'
+}
+const logWeather = ({date,weather} : {date : Date; weather : string}) : void => {
+    console.log(date);
+    console.log(weather);
 }
