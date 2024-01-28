2a505b73e2c8fb67de2e39eb13184a8c0e7fe39b
abyankamal <kamalmuhammad414@gmail.com>
2024-01-28 22:18:19 +0700

    Commit Keduapuluhtiga

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({name: "John", age: 28});
+const user = new User({id: "a969"});
 
-user.on("change", () => {
-    console.log("Change 1");
-});
+user.fetch();
 
-user.on("change", () => {
-    console.log("Change 2");
-});
-
-user.trigger("change");
+setTimeout(() => {
+    console.log(user);
+}, 5000);
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,7 @@
+import axios , {AxiosResponse} from "axios";
+
 interface UserProps {
+    id?: string | number;
     name?: string;
     age?: number;
 }
@@ -28,5 +31,22 @@ export class User {
         if(!handlers || handlers.length === 0) return;
 
         handlers.forEach(handler => handler());
+    }
+
+    fetch(): void {
+        axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
+          (response: AxiosResponse) => {
+            this.set(response.data);
+          }
+        );
+      }
+
+    save() : void {
+        const id = this.get('id');
+        if(id){
+            axios.put(`http://localhost:3000/users/${id}`, this.data);
+        }else {
+            axios.post(`http://localhost:3000/users`, this.data);
+        }
     }
 }
