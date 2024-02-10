295a49a27ad6eeef0510266aa8efaf5cdb8f08cb
abyankamal <kamalmuhammad414@gmail.com>
2024-02-10 22:49:31 +0700

    Commit Tigapuluhlima

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,5 +10,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log('Listening on this porte : 3000');
 });
